1780af5eff4baaf91071de9549d63d190ca9f020
AlanSantos1 <alan.informas@gmail.com>
2020-05-13 18:13:34 +0000

    CA2 on CA3 , CA3 ready to start

---
+++ mobdev-ca3/src/app/pages/deaths/deaths.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { NavController } from '@ionic/angular';
+import { ApiService } from '../../services/api.service';
+
+
+@Component({
+  selector: 'app-deaths',
+  templateUrl: './deaths.page.html',
+  styleUrls: ['./deaths.page.scss'],
+})
+export class DeathsPage implements OnInit {
+
+    deaths: Observable<any>;
+
+  constructor(private router: Router,private api:ApiService) { }
+
+ ngOnInit() {
+      this.deaths = this.api.getDeaths();
+  this.deaths.subscribe(data => {
+      console.log('my deaths: ', data);
+    });
+
+}
+openDetails(death){
+      let deathId = death.death_id
+      this.router.navigateByUrl(`/tabs/deaths/${deathId}`);
+  }
+}
